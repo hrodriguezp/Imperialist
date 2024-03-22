0762ef62ba9a48de7f5a33d290d0fd3e703733b0
arodrigoch <arodrigoch@educacion.com>
2024-03-22 12:41:42 +0100

    He tenido que volver a crear y pensar el metodo load world desde 0 porque no conseguia que funcionase, ahora ya parece que esta correcto

--- src/main/java/Mapa.java
+++ src/main/java/Mapa.java
@@ -4,43 +4,70 @@ import java.util.*;
 public class Mapa {
 
     private Map<String, Territorio> lista;
+    private Map<String, List<String>> nombres;
+    private ArrayList<String> tPrincipal;
 
 
-   public Mapa() {
+    public Mapa() {
        this.lista = new LinkedHashMap<>();
+       this.nombres = new LinkedHashMap<>();
+       this.tPrincipal = new ArrayList<>();
        loadWorld();
    }
 
-    public void loadWorld() {
+//    public void loadWorld() {
+//        String line;
+//        File file = new File(Ctes.PATH_TERRITORIOS);
+//        HashMap<String, ArrayList<String>> conVecinos = new HashMap<>();
+//        ArrayList<String> terrPrim = new ArrayList<>();
+//        try {
+//            BufferedReader reader = new BufferedReader(new FileReader(file));
+//            while ( (line = reader.readLine()) != null ) {
+//                if (line.isEmpty()) { continue; }
+//                String[] a = line.split(Ctes.EXP_TERRITORIO_SPLITTER);
+//
+//                String [] st = a[0].split(":");
+//                String territorioPrincipal = st[0];
+//                Territorio territorioPrin = new Territorio(territorioPrincipal);
+//                String[] vecinos = st[1].split(", ");
+//                ArrayList<String> vecinosTOLIST = new ArrayList<>(Arrays.stream(vecinos).toList());
+//                terrPrim.add(territorioPrincipal);
+//                conVecinos.put(territorioPrincipal ,vecinosTOLIST);
+//                lista.put(territorioPrincipal,territorioPrin);
+//            }
+//            for (int i = 0; i < lista.size(); i++) {
+//                String nTerritorio = terrPrim.get(i);
+//                ArrayList<String> vecinosSt = conVecinos.get(nTerritorio);
+//                ArrayList<Territorio> vecinos = new ArrayList<>();
+//                for (int j = 0; j < vecinosSt.size(); j++) {
+//                    vecinos.add(lista.get(vecinosSt.get(i)));
+//                }
+//                lista.get(nTerritorio).setVecinos(vecinos);
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    public void cargarNombres() {
         String line;
         File file = new File(Ctes.PATH_TERRITORIOS);
-        HashMap<String, ArrayList<String>> conVecinos = new HashMap<>();
-        ArrayList<String> terrPrim = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             while ( (line = reader.readLine()) != null ) {
                 if (line.isEmpty()) { continue; }
                 String[] a = line.split(Ctes.EXP_TERRITORIO_SPLITTER);
-
+                // ...
                 String [] st = a[0].split(":");
                 String territorioPrincipal = st[0];
-                Territorio territorioPrin = new Territorio(territorioPrincipal);
+                tPrincipal.add(territorioPrincipal);
                 String[] vecinos = st[1].split(", ");
-                ArrayList<String> vecinosTOLIST = new ArrayList<>(Arrays.stream(vecinos).toList());
-                terrPrim.add(territorioPrincipal);
-                conVecinos.put(territorioPrincipal ,vecinosTOLIST);
-                lista.put(territorioPrincipal,territorioPrin);
+                List<String> vecinosTOLIST = new ArrayList<>(Arrays.stream(vecinos).toList());
+                nombres.put(territorioPrincipal,vecinosTOLIST);
             }
-            for (int i = 0; i < lista.size(); i++) {
-                String nTerritorio = terrPrim.get(i);
-                ArrayList<String> vecinosSt = conVecinos.get(nTerritorio);
-                ArrayList<Territorio> vecinos = new ArrayList<>();
-                for (int j = 0; j < vecinosSt.size(); j++) {
-                    vecinos.add(lista.get(vecinosSt.get(i)));
-                }
-                lista.get(nTerritorio).setVecinos(vecinos);
-            }
-
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -48,29 +75,45 @@ public class Mapa {
         }
     }
 
-//    public void agregarTerritorio(String territorioPrincipal, List<String> vecinos) {
-//        lista.put(territorioPrincipal, new ArrayList<>(vecinos));
-//    }
-//
-//    public List<String> getTerritorios() {
-//        return new ArrayList<>(lista.keySet());
-//    }
-//
-//    public ArrayList<String> getVecinos(String territorio){
-//       return (ArrayList<String>) lista.get(territorio);
-//    }
-//    @Override
-//    public String toString() {
-//        return lista.toString();
-//    }
-//    public void pintar(){
-//        for (String s : lista.keySet()) {
-//            System.out.println(lista.get(s));
-//        }
-//    }
+    public void loadWorld(){
+        cargarNombres();
+        for (int i = 0; i < nombres.size(); i++) {
+            Territorio territorio = new Territorio(tPrincipal.get(i));
+            lista.put(tPrincipal.get(i),territorio);
+        }
+        for (int i = 0; i <lista.size(); i++) {
+            List<String> vString = nombres.get(tPrincipal.get(i));
+            ArrayList<Territorio> vecinos = new ArrayList<>();
+            for (int j = 0; j < vString.size(); j++) {
+                vecinos.add(lista.get(vString.get(j)));
+            }
+            lista.get(tPrincipal.get(i)).setVecinos(vecinos);
+        }
+        System.out.println(nombres.toString());
+    }
+
+    public void agregarTerritorio(String territorioPrincipal, List<String> vecinos) {
+        nombres.put(territorioPrincipal, new ArrayList<>(vecinos));
+    }
+
+    public List<String> getTerritorios() {
+        return new ArrayList<>(nombres.keySet());
+    }
+
+    public ArrayList<String> getVecinos(String territorio){
+       return (ArrayList<String>) nombres.get(territorio);
+    }
+    @Override
+    public String toString() {
+        return nombres.toString();
+    }
+    public void pintar(){
+        for (String s : nombres.keySet()) {
+            System.out.println(nombres.get(s));
+        }
+    }
 
     public static void main(String[] args) {
         Mapa mapa = new Mapa();
-        mapa.loadWorld();
     }
     }
