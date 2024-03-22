4a228d9c75492a9653ea0f6b2957b7ec48100245
sergiooalonsoo <salonsogon@educacion.navarra.es>
2024-03-22 10:19:38 +0100

    Inicio de la clase main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola, soy hugo");
-        System.out.println("hola, este es nuestro proyecto");
+        Juego juego = new Juego();
+        juego.jugar();
     }
 }
